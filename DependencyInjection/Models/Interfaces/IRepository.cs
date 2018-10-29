10096733f2463c3abd0d1902c5639bd72fbdcd19
Jon Perkins <captainperk81@gmail.com>
2018-10-29 06:57:33 -0400

    Creating the Model and Repository

---
+++ DependencyInjection/Models/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace DependencyInjection.Models.Interfaces
+{
+    public interface IRepository
+    {
+        IEnumerable<Product> Products { get; }
+        Product this[string name] { get; }
+
+        void AddProduct(Product product);
+        void DeleteProduct(Product product);
+    }
+}
