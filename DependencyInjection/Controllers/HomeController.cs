ac84a726a7e14dff464fe18f0a5293697a2bbcf7
Jon Perkins <captainperk81@gmail.com>
2018-10-30 06:55:09 -0400

    Preparing for Dependency Injection

--- DependencyInjection/Controllers/HomeController.cs
+++ DependencyInjection/Controllers/HomeController.cs
@@ -1,13 +1,17 @@
-﻿using DependencyInjection.Infrastructure;
-using DependencyInjection.Models.Interfaces;
+﻿using DependencyInjection.Models.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DependencyInjection.Controllers
 {
     public class HomeController : Controller
     {
-        public IRepository Repository { get; set; } = TypeBroker.Repository;
+        private readonly IRepository _repository;
 
-        public ViewResult Index() => View(Repository.Products);
+        public HomeController(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ViewResult Index() => View(_repository.Products);
     }
 }
