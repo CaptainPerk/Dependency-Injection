b5a0f6bda7fdb7c7bfb518bf575ffd82d41e5f30
Jon Perkins <captainperk81@gmail.com>
2018-10-30 07:04:26 -0400

    Unit Testing a Controller with a Dependency

---
+++ DependencyInjection_Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using DependencyInjection.Controllers;
+using DependencyInjection.Models;
+using DependencyInjection.Models.Interfaces;
+using Moq;
+using Xunit;
+
+namespace DependencyInjection_Tests.Controllers
+{
+    public class HomeControllerTests
+    {
+        [Fact(DisplayName = "When the Index action is called the correct product data is sent to the View")]
+        public void ControllerHasCorrectData()
+        {
+            // Arrange
+            var data = new[] {new Product {Name = "Test", Price = 100M}};
+            var _mockRepository = new Mock<IRepository>();
+            _mockRepository.Setup(r => r.Products).Returns(data);
+            df;
+            kjsdf;
+            var homeController = new HomeController(_mockRepository.Object);
+
+            // Act
+            var viewResult = homeController.Index();
+
+            // Assert
+            Assert.Equal(data, viewResult.ViewData.Model);
+        }
+    }
+}
