6a4cec330e042c7e82428ddf5b73ab75d810ef54
Jon Perkins <captainperk81@gmail.com>
2018-10-30 07:17:09 -0400

    Using Dependency Chains

--- DependencyInjection/Startup.cs
+++ DependencyInjection/Startup.cs
@@ -11,6 +11,7 @@ namespace DependencyInjection
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddTransient<IRepository, MemoryRepository>();
+            services.AddTransient<IModelStorage, DictionaryStorage>();
             services.AddMvc();
         }
 
