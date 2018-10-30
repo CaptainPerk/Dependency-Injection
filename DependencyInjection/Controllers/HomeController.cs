1519a336bbe58bf36a7ab697435b4f3a10862d8a
Jon Perkins <captainperk81@gmail.com>
2018-10-30 18:59:49 -0400

    Using Dependency Injection for Concrete Types

--- DependencyInjection/Controllers/HomeController.cs
+++ DependencyInjection/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using DependencyInjection.Models.Interfaces;
+﻿using DependencyInjection.Models;
+using DependencyInjection.Models.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DependencyInjection.Controllers
@@ -6,12 +7,18 @@ namespace DependencyInjection.Controllers
     public class HomeController : Controller
     {
         private readonly IRepository _repository;
+        private readonly ProductTotalizer _productTotalizer;
 
-        public HomeController(IRepository repository)
+        public HomeController(IRepository repository, ProductTotalizer productTotalizer)
         {
             _repository = repository;
+            _productTotalizer = productTotalizer;
         }
 
-        public ViewResult Index() => View(_repository.Products);
+        public ViewResult Index()
+        {
+            ViewBag.Total = _productTotalizer.Total;
+            return View(_repository.Products);
+        }
     }
 }
--- DependencyInjection/Startup.cs
+++ DependencyInjection/Startup.cs
@@ -12,6 +12,7 @@ namespace DependencyInjection
         {
             services.AddTransient<IRepository, MemoryRepository>();
             services.AddTransient<IModelStorage, DictionaryStorage>();
+            services.AddTransient<ProductTotalizer>();
             services.AddMvc();
         }
 
